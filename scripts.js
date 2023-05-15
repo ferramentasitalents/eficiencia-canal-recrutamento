4690871b76704d3a358df3037c881d10f6ba016f
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-15 16:53:30 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateEfficiency() {
+  const numCandidatos = parseFloat(document.getElementById("num-candidatos").value);
+  const numAplicantes = parseFloat(document.getElementById("num-aplicantes").value);
+  const efficiency = (numCandidatos / numAplicantes) * 100;
+  document.getElementById("result").innerHTML = `Eficiência do canal de recrutamento: ${efficiency.toFixed(2)}%`;
+}
 
